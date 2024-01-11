daf8d6fc06cb0921c546974356e4ad65e0bb7714
Anubis Zero <crawfordmaxwell24@gmail.com>
2024-01-10 20:03:16 -0500

    feat: redirect to ZDA page path via App Router navigation (#2)

---
+++ app/commissions/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Redirector from "../Redirector";
+
+const CommissionsPage = () => {
+  return (
+    <>
+      <Redirector page="commissions" />
+    </>
+  );
+};
+export default CommissionsPage;
