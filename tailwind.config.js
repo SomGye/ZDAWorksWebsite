85a21b86048455fbc6bc9afae025f6d41821521d
SomGye <crawfordmaxwell24@gmail.com>
2024-01-17 20:08:22 -0500

    feat: add lighterCard, zdaBlue 450 1000, animations for dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
           darkCard: '#0f0e11',
           light: '#fafafa',
           lightCard: '#f0f0f0',
+          lighterCard: '#f5f5f5',
         },
         zdaText: {
           dark: '#19141a',
@@ -52,12 +53,14 @@ export default {
           200: '#b7ddff',
           300: '#85c8ff',
           400: '#4ca9ff',
+          450: '#49a1ff',
           500: '#2284ff',
           600: '#0b62ff',
           700: '#044af1',
           800: '#0b3cc2',
           900: '#0f3590',
           950: '#0f245c',
+          1000: '#0a172e',
         }
       },
       dropShadow: {
@@ -68,7 +71,31 @@ export default {
       },
       fontFamily: {
         'outfit': 'Outfit',
-      }
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          from: { opacity: 0, transform: 'translateX(2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          from: { opacity: 0, transform: 'translateX(-2px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
     screens: {
       'xxs': '320px',
