d04de5f8d1275fa30dc6516bea4cd51fb3cdd925
SomGye <crawfordmaxwell24@gmail.com>
2024-01-17 21:16:03 -0500

    feat: split out Icons to file, add balance wrap to Body subtext

--- src/sections/Body/Body.tsx
+++ src/sections/Body/Body.tsx
@@ -3,6 +3,7 @@ import { useRecoilValue } from "recoil";
 import { themeAtom } from "../../states/ThemeAtom";
 import zdaWorks_dark from "/zdaworks_dark.svg";
 import zdaWorks_light from "/zdaworks_light.svg";
+import { rightArrowBigIcon } from "../../icons";
 
 const Body = () => {
   const theme = useRecoilValue(themeAtom);
@@ -132,24 +133,11 @@ const Body = () => {
           >
             Ko-fi{" "}
             <span className="inline-block mx-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              {rightArrowBigIcon}
             </span>
           </h2>
           <p
-            className={`m-0 text-sm tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
+            className={`m-0 text-sm text-balance tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
           >
             Download high-quality versions of <br />
             ZDA Media for free
@@ -166,24 +154,11 @@ const Body = () => {
           >
             ZDA Website{" "}
             <span className="inline-block mx-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              {rightArrowBigIcon}
             </span>
           </h2>
           <p
-            className={`m-0 max-w-[30ch] text-sm tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
+            className={`m-0 max-w-[30ch] text-sm text-balance tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
           >
             Home of ZDA&apos;s art portfolio <br />
             and commission info
@@ -200,24 +175,11 @@ const Body = () => {
           >
             Print Shop{" "}
             <span className="inline-block mx-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              {rightArrowBigIcon}
             </span>
           </h2>
           <p
-            className={`m-0 max-w-[30ch] text-sm tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
+            className={`m-0 max-w-[30ch] text-sm text-balance tracking-wide opacity-75 dark:opacity-60 text-zdaText-darker dark:text-zdaText-lighter select-none`}
           >
             Get art prints, posters, phone cases, stickers and more
           </p>
