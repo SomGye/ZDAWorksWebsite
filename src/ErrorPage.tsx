e2bc87a2616b02b3519b35deecb54cb8811fbc3e
SomGye <crawfordmaxwell24@gmail.com>
2024-01-19 22:24:34 -0500

    fix: no select in ErrorPage

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -13,13 +13,13 @@ const ErrorPage = () => {
   return (
     <div className="flex justify-center w-full h-full">
       <div className="flex items-center">
-        <span className="inline-flex m-4 font-semibold text-xl text-gray-300">
+        <span className="inline-flex m-4 font-semibold text-xl text-gray-300 pointer-events-none select-none">
           404{" "}
           <div className="mx-2 border-l-2 border-gray-300/25 border-opacity-70" />{" "}
           Page Not Found
         </span>
         <button
-          className="inline-flex items-center pt-[6px] pb-[6px] pl-4 pr-4 rounded-md bg-zdaBlue-600 text-[15px] text-gray-200 font-medium transition ease-out duration-300 motion-reduce:transition-none hover:text-gray-100 hover:bg-zdaBlue-800"
+          className="inline-flex items-center pt-[6px] pb-[6px] pl-4 pr-4 rounded-md bg-zdaBlue-600 text-[15px] text-gray-200 font-medium transition ease-out duration-300 motion-reduce:transition-none hover:text-gray-100 hover:bg-zdaBlue-800 select-none"
           onClick={() => window.location.replace("/")}
         >
           <span className="inline-block mr-2 transition-transform group-hover:-translate-x-1 motion-reduce:transform-none">
