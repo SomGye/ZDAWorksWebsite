7f559aedc374a773695c2c509f25ae836a212525
SomGye <crawfordmaxwell24@gmail.com>
2024-01-22 16:13:06 -0500

    fix: apply theming on ErrorPage too

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { leftArrowIcon } from "./icons";
+import ThemeMenu from "./ThemeMenu";
 
 const ErrorPage = () => {
   React.useEffect(() => {
@@ -11,11 +12,15 @@ const ErrorPage = () => {
   }, []);
 
   return (
-    <div className="flex justify-center w-full h-full">
+    <div className="flex justify-center w-full h-full bg-zdaBG-light dark:bg-zdaBG-dark">
+      {/* NOTE: ThemeMenu needed here to detect theme separately, since this page bypasses App */}
+      <div className="cursor-pointer absolute top-0 left-1/2 mr-0 mt-3 lg:left-auto lg:right-0 lg:mr-2">
+        <ThemeMenu />
+      </div>
       <div className="flex items-center">
-        <span className="inline-flex m-4 font-semibold text-xl text-gray-300 pointer-events-none select-none">
+        <span className="inline-flex m-4 font-semibold text-xl text-gray-600 dark:text-gray-300 pointer-events-none select-none">
           404{" "}
-          <div className="mx-2 border-l-2 border-gray-300/25 border-opacity-70" />{" "}
+          <div className="mx-2 border-l-2 border-gray-600/25 dark:border-gray-300/25 border-opacity-70" />{" "}
           Page Not Found
         </span>
         <button
