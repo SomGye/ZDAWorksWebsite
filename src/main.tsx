60a5ea8a4c3ec8bc60c723bc56aa24b4db22d2b5
SomGye <crawfordmaxwell24@gmail.com>
2024-01-26 22:22:47 -0500

    tweak: only import needed fonts

--- src/main.tsx
+++ src/main.tsx
@@ -7,28 +7,28 @@ import App from "./App.tsx";
 import "./index.css";
 
 /* FONTSOURCE IMPORTS */
-import "@fontsource/outfit/100.css";
-import "@fontsource/outfit/200.css";
+// import "@fontsource/outfit/100.css";
+// import "@fontsource/outfit/200.css";
 import "@fontsource/outfit/300.css";
-import "@fontsource/outfit/400.css";
+// import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/500.css";
-import "@fontsource/outfit/600.css";
-import "@fontsource/outfit/700.css";
-import "@fontsource/outfit/800.css";
-import "@fontsource/outfit/900.css";
+// import "@fontsource/outfit/600.css";
+// import "@fontsource/outfit/700.css";
+// import "@fontsource/outfit/800.css";
+// import "@fontsource/outfit/900.css";
 
-import "@fontsource/plus-jakarta-sans/200-italic.css";
-import "@fontsource/plus-jakarta-sans/200.css";
+// import "@fontsource/plus-jakarta-sans/200-italic.css";
+// import "@fontsource/plus-jakarta-sans/200.css";
 import "@fontsource/plus-jakarta-sans/300-italic.css";
-import "@fontsource/plus-jakarta-sans/300.css";
-import "@fontsource/plus-jakarta-sans/400-italic.css";
+// import "@fontsource/plus-jakarta-sans/300.css";
+// import "@fontsource/plus-jakarta-sans/400-italic.css";
 import "@fontsource/plus-jakarta-sans/400.css";
-import "@fontsource/plus-jakarta-sans/500-italic.css";
-import "@fontsource/plus-jakarta-sans/500.css";
-import "@fontsource/plus-jakarta-sans/600-italic.css";
+// import "@fontsource/plus-jakarta-sans/500-italic.css";
+// import "@fontsource/plus-jakarta-sans/500.css";
+// import "@fontsource/plus-jakarta-sans/600-italic.css";
 import "@fontsource/plus-jakarta-sans/600.css";
-import "@fontsource/plus-jakarta-sans/700-italic.css";
-import "@fontsource/plus-jakarta-sans/700.css";
+// import "@fontsource/plus-jakarta-sans/700-italic.css";
+// import "@fontsource/plus-jakarta-sans/700.css";
 
 const router = createBrowserRouter([
   {
